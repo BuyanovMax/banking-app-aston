f5504966b535993cf870d4d89b8edf678d961aeb
Макс <maxim__90.90@mail.ru>
2023-09-21 14:12:25 +0400

    firstCommit

---
+++ src/main/java/com/example/bankingappaston/BankingAppAstonApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankingappaston;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAppAstonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingAppAstonApplication.class, args);
+    }
+
+}
